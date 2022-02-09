ff14f9ce44e2c4bf73ba9b1cf3d8e2f8023becdc
Literally Collective <bluedragon.cstar@outlook.com>
2022-02-09 11:36:17 -0300

    Update config.ts

--- src/widgets/Menu/config.ts
+++ src/widgets/Menu/config.ts
@@ -144,7 +144,7 @@ export const socials = [
   {
     label: "Twitter",
     icon: "TwitterIcon",
-    href: "https://twitter.com/",
+    href: "https://twitter.com/dearMoon_X",
   },
   // {
   //   label: "Discord",
@@ -154,17 +154,17 @@ export const socials = [
   {
     label: "Facebook",
     icon: "FacebookIcon",
-    href: "https://www.facebook.com/",
+    href: "https://www.facebook.com/dearmoonx",
   },
   {
     label: "Telegram",
     icon: "TelegramIcon",
-    href: "https://t.me/",
+    href: "https://t.me/dearmoonx_official",
   },
   {
     label: "GitHub",
     icon: "GitHubIcon",
-    href: "https://github.com/",
+    href: "https://github.com/dmx-finance",
   },
   {
     label: "Medium",
@@ -177,12 +177,12 @@ export const infos = [
   {
     label: "Nomics",
     icon: "NomicsIcon",
-    href: "https://nomics.com/assets/",
+    href: "https://nomics.com/assets/DMX6-dearMoonX?utm_source=twitter&utm_medium=bot&utm_campaign=new_asset&utm_content=dmx6&d=1643756995",
   },
   {
     label: "Website",
     icon: "WebsiteIcon",
-    href: "https://www.spacex.com//",
+    href: "https://www.dearmoonx.com/",
   },
   {
     label: "CBanks",
